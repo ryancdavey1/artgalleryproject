f32e346f2b91ec381c62a2acc393f2852c9a1bc2
ryancdavey1 <ryancdavey1@gmail.com>
2020-10-24 01:03:07 -0600

    add session, user controller functions, add session views

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ApplicationRecord
   has_secure_password
+
+  #loading signup form
+  def new
+  end
+
+  #sign up the user
+  def create
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+
+  root 'sessions#home'
   get 'signup' => 'users#new'
   get 'login' => 'sessions#new'
   post 'login' => 'sessions#create'
