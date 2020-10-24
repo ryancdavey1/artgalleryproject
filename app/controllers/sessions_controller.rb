71d85aebcc9a298e775cb18705121e5283adc31d
ryancdavey1 <ryancdavey1@gmail.com>
2020-10-24 02:49:10 -0600

    add login session action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,13 @@ class SessionsController < ApplicationController
   end
 
   def create
-
+    @user = User.find_by(email: params[:user][:email])
+    if @user && @user.authenticate(params[:user][:password])
+      session[:user_id] = @user.id
+      redirect_to user_path
+    else
+      redirect_to 'login'
+    end
   end
 
   def home
