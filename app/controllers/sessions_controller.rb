9ae6208513f5cb836645495ffe6d52f46c890203
ryancdavey1 <ryancdavey1@gmail.com>
2020-10-24 02:33:38 -0600

    add session routes and views

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,19 @@
 class SessionsController < ApplicationController
   def new
+    @user = User.new
+    render :login
   end
 
   def create
+
   end
 
   def home
+  end
+
+  def destroy
+    session.clear
+    redirect_to '/'
   end
 
   
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   get 'signup' => 'users#new'
   get 'login' => 'sessions#new'
   post 'login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
   resources :users
 end
