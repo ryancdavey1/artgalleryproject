f162acdb4a119a4f31369faaae9813cfd6920bf5
ryancdavey1 <ryancdavey1@gmail.com>
2020-11-02 23:32:05 -0700

    small changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,11 @@
 class ApplicationController < ActionController::Base
   helper_method :current_user
   before_action :redirect_if_not_logged_in
-  #helper_method :user_is_authenticated
 
   private
 
   def redirect_if_not_logged_in
     if !logged_in?
-      #flash[:error] = "You must be logged in to see this page!"
       redirect_to '/' 
     end
   end
